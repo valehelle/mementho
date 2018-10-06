2cabfea8f157378dfdd4667f86e3b792001a4a74
valehelle <hazmiirfan92@gmail.com>
2018-10-06 21:37:15 +0800

    change url of host

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :mementho, MementhoWeb.Endpoint,
   http: [port: 4000],
-  url: [host: "example.com", port: 80],
+  url: [host: "owllang.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   code_reloader: false
