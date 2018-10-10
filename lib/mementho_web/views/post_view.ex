70201bf874e3f776454d327e8f5c8d57eef15cdb
valehelle <hazmiirfan92@gmail.com>
2018-10-10 17:09:18 +0800

    show link

--- lib/mementho_web/views/post_view.ex
+++ lib/mementho_web/views/post_view.ex
@@ -6,4 +6,12 @@ defmodule MementhoWeb.PostView do
     Timex.from_now(date)
   end
 
+  def generateHtml(text) do
+    text
+    |> text_to_html()
+    |> safe_to_string
+    |> String.replace(~r/https?:\/\/[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b[-a-zA-Z0-9@:%_\+.~#?&\/\/=]*/, "<a href =\\0> \\0 </a>")
+    |> text_to_html([attributes: [style: "color:black;  margin-top: 10px; font-size:11pt; margin-bottom: 2px; line-height: 19px;"],escape: false])
+  end
+
 end
