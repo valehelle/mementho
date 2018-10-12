b1ed30a236c98743c6544789a65dcde844dd9a4d
valehelle <hazmiirfan92@gmail.com>
2018-10-12 09:55:28 +0800

    fix bold

--- lib/mementho_web/views/post_view.ex
+++ lib/mementho_web/views/post_view.ex
@@ -11,7 +11,7 @@ defmodule MementhoWeb.PostView do
     |> text_to_html(wrapper_tag: :div)
     |> safe_to_string
     |> String.replace(~r/https?:\/\/[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b[-a-zA-Z0-9@:%_\+.~#?&\/\/=]*/, "<a href =\\0> \\0 </a>") 
-    |> String.replace(~r/\*\*.*\*\*/,"<b>\\0</b>")
+    |> String.replace(~r/\*\*[\s\S]+\*\*/,"<b>\\0</b>")
     |> String.replace(~r/\*\*/,"")
     |> text_to_html([attributes: [style: "color:black;  font-size:12pt; margin-bottom: 2px;"],escape: false, wrapper_tag: :div])
   end
