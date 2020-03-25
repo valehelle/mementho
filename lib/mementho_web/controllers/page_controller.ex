70c6cb0342379cc469206130bb75c3e7ec19f961
valehelle <hazmiirfan92@gmail.com>
2020-03-25 23:34:20 +0800

    redirect to location page

--- lib/mementho_web/controllers/page_controller.ex
+++ lib/mementho_web/controllers/page_controller.ex
@@ -5,7 +5,13 @@ defmodule MementhoWeb.PageController do
   alias Mementho.Accounts.User
   def index(conn, _params) do
     changeset = Accounts.change_user(%User{})
-    conn
-    |> render("index.html", changeset: changeset, action: Routes.user_path(conn, :register_user))
+    IO.inspect  Guardian.Plug.current_resource(conn)
+    case Guardian.Plug.current_resource(conn) do
+      nil ->  
+        conn
+        |> render("index.html", changeset: changeset, action: Routes.user_path(conn, :register_user))
+      user -> redirect(conn, to: "/location")
+
+    end
   end
 end
